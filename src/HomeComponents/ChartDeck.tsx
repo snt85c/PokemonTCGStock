b6829c09a49216025f57a7a0298e3ae408e82767
snt85c <santi.cacciola@hotmail.it>
2022-11-04 13:12:33 +0000

    CLEANUP: removes commentation, add comments to explain logic, removes obsolete code

--- src/HomeComponents/ChartDeck.tsx
+++ src/HomeComponents/ChartDeck.tsx
@@ -21,41 +21,21 @@ export default function ChartDeck(props: { deckId: string }) {
   const { user } = useUserAuth();
   const isDarkMode = useProfileStore((state: iState) => state.darkmode);
   const rate = useProfileStore((state: iState) => state.conversionRate);
-  // const [isReady,setIsReady] = useState(false)
   const userDeckValue = useCollectionStore(
     (state: iCollectionStore) => state.collectionValue
   );
-
   const userDeckInfo = useCollectionStore(
     (state: iCollectionStore) => state.currentDeckInfo
   );
   const isReady = useRef(false);
   const [chart, setChart] = useState([
     {
-      label: "temp",
+      label: "",
       data: [
-        { value: 0, date: new Date() },
         { value: 0, date: new Date() },
       ],
     },
   ]);
-
-  const cardValue = (card: iCard) => {
-    /**
-     * when calculating the chart, i need to append the latest value at the end which might be different from
-     * the one i have historically due to live user interaction. i call this function to calculate the values
-     * currently stored
-     */
-    let result = Object.keys(card.userDeckInfo.quantity).reduce(
-      (prev, current) =>
-        prev +
-        card.userDeckInfo.quantity[current] *
-          card.tcgplayer.prices[current].market,
-      0
-    );
-
-    return result;
-  };
 
   useEffect(() => {
     async function ttt() {
@@ -72,13 +52,13 @@ export default function ChartDeck(props: { deckId: string }) {
       let date: Date = new Date();
       const querySnapshot = await getDocs(queryRef);
       if (querySnapshot.size !== 0) {
-        //i query all the historical instances of this collection, 
+        //i query all the historical instances of this collection,
         querySnapshot.forEach((instance) => {
           let keys: string[] = [];
           let instanceData = instance.data();
           chartdata.label = props.deckId;
           instanceData.result.forEach((item: any) => {
-            // for each i get all the keys from the prices , 
+            // for each i get all the keys from the prices ,
             dayValueOfDeck = 0;
             Object.keys(item.prices).map((cardType) => {
               if (!keys.includes(cardType)) keys.push(cardType);
@@ -89,9 +69,9 @@ export default function ChartDeck(props: { deckId: string }) {
                   item.prices[key] &&
                   item.prices[key].market &&
                   item.quantity[key] !== 0
-                  ) {
-                    // then for each key i sum and multiply each card for its price and quantity
-                    dayValueOfDeck +=
+                ) {
+                  // then for each key i sum and multiply each card for its price and quantity
+                  dayValueOfDeck +=
                     item.prices[key].market * item.quantity[key];
                   date = item.date.toDate();
                 }
@@ -105,45 +85,29 @@ export default function ChartDeck(props: { deckId: string }) {
           });
         });
         chartdata.data.sort((a, b) => {
-          //sort it by Date, as Firebase doesn't ensure that the data will be saved chronologically in the first place, otherwise i might get the wrong amount in the wrong place
+          /**
+           *sort it by Date, as Firebase doesn't ensure that the data will be saved chronologically
+           *, otherwise i might get the wrong amount in the wrong place
+           */
           return Number(a.date) - Number(b.date);
         });
-        // const queryRef = collection(
-        //   db,
-        //   "users",
-        //   user.uid,
-        //   "decks",
-        //   props.deckId,
-        //   "cards"
-        // );
-        //to make the chart dynamic, i take the current deck as saved on firebase and i append it
-        // at the end of chartdata(i dont need to sort in this case)
-        // const queryCurrentSnapshot = await getDocs(queryRef);
-        let currentDeck: iData = { value: 0, date: new Date() };
-        // queryCurrentSnapshot.forEach((item) => {
-          // let temp: iCard = item.data() as iCard;
-          if(props.deckId === userDeckInfo.id){
-            currentDeck = {
-              ...currentDeck,
-              value: userDeckValue * rate,
-            }; 
-            chartdata.data.push(currentDeck);
-          }
-          // else{
-          //   currentDeck = {
-          //     ...currentDeck,
-          //     value: currentDeck.value + cardValue(temp) * rate,
-          //   };
-
-          // }
-
-        // });
+        if (props.deckId === userDeckInfo.id) {
+          /**
+           *to make the chart dynamic, i take the current deck as saved on zustand and i append it
+           *at the end of chartdata(i dont need to sort in this case, as it's clearly going
+           *to be pushed at the end)
+           */
+          chartdata.data.push({
+            date: new Date(),
+            value: userDeckValue * rate,
+          });
+        }
         setChart([chartdata]);
       }
     }
     ttt();
   }, [userDeckValue, userDeckInfo]);
-  
+
   if (chart[0].data.length > 2 && rate > 0) {
     isReady.current = true;
   }
@@ -173,30 +137,28 @@ export default function ChartDeck(props: { deckId: string }) {
 
   return (
     <>
-      {
-        /*chart[0].label !== "temp" && chart[0].data[0].value > 0 && */ isReady.current ? (
-          <Chart
-            options={{
-              data: chart,
-              primaryAxis,
-              secondaryAxes,
-              dark: isDarkMode,
-              tooltip: false,
-            }}
-          />
-        ) : (
-          <div className="flex flex-col justify-center items-center h-full">
-            <div>
-              <div className="flex justify-center items-center text-amber-500 font-bold  leading-none">
-                no data
-              </div>
-              <div className="flex justify-center items-center text-[0.6rem] leading-none">
-                no historical data available
-              </div>
+      {isReady.current ? (
+        <Chart
+          options={{
+            data: chart,
+            primaryAxis,
+            secondaryAxes,
+            dark: isDarkMode,
+            tooltip: false,
+          }}
+        />
+      ) : (
+        <div className="flex flex-col justify-center items-center h-full">
+          <div>
+            <div className="flex justify-center items-center text-amber-500 font-bold  leading-none">
+              no data
+            </div>
+            <div className="flex justify-center items-center text-[0.6rem] leading-none">
+              no historical data available
             </div>
           </div>
-        )
-      }
+        </div>
+      )}
     </>
   );
 }
